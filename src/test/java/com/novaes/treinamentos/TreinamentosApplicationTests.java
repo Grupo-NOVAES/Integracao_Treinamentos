db2d5882a9550f98198aff2c0d42c44f75fd515c
Bruno <124815320+Brxnin-FX@users.noreply.github.com>
2024-09-20 14:16:48 -0300

    Add files via upload

---
+++ src/test/java/com/novaes/treinamentos/TreinamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novaes.treinamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
