29112438267ae5076bd3fe9239bf78e1719deab9
Brxnin-FX <brunosantiago7002@gmail.com>
2024-10-04 08:52:37 -0300

    again

--- src/test/java/com/novaes/treinamentos/TreinamentosApplicationTests.java
+++ src/test/java/com/novaes/treinamentos/TreinamentosApplicationTests.java
@@ -6,9 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TreinamentosApplicationTests {
 
-	//We don't have any tests to be done yet.
+	
 	@Test
 	void contextLoads() {
+		//We don't have any tests to be done yet.
 	}
 
 }
