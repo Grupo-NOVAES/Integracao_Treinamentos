41525582fa07253649c0a519b3d3800e99db5b3c
Brxnin-FX <brunosantiago7002@gmail.com>
2024-10-04 08:51:12 -0300

    adding comment in test main

--- src/test/java/com/novaes/treinamentos/TreinamentosApplicationTests.java
+++ src/test/java/com/novaes/treinamentos/TreinamentosApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TreinamentosApplicationTests {
 
+	//We don't have any tests to be done yet.
 	@Test
 	void contextLoads() {
 	}
