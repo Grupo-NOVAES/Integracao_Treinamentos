4e02a615b8921d17998ca0f74d4daa29cc1e3cf6
Brxnin-FX <brunosantiago7002@gmail.com>
2024-12-05 10:37:58 -0300

    view respoonses of user

--- src/main/resources/static/js/manager/userData.js
+++ src/main/resources/static/js/manager/userData.js
@@ -1,4 +1,20 @@
 const userId = document.getElementById("userId").value;
+const linkResponseButtons = document.querySelectorAll(".linkResponseList");
+
+linkResponseButtons.forEach(button => {
+  button.addEventListener('click', function () {
+    const idUser = this.getAttribute("data-iduser");
+    const nrNumber = this.getAttribute("data-nrnumber");
+    
+    goToListResponses(idUser, nrNumber);
+  });
+});
+
+function goToListResponses(idUser, nrNumber) {
+  window.location.href = `/user/infoClient/${idUser}/${nrNumber}`;
+}
+
+
 async function activeUser(){
     console.log("função chamada!")
     let response = await fetch(`http://localhost:8080/user/activeUser/${userId}`, { 
@@ -7,3 +23,4 @@ async function activeUser(){
     window.location.reload();
     console.log("res: "+JSON.stringify(response))
 }
+
