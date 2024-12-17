1cc9699a5d1942438d26498ea181bb6ec369f01e
luigigh <luigifalconibruno@gmail.com>
2024-12-17 10:13:08 -0300

    uderData Options changes

--- src/main/resources/static/js/manager/userData.js
+++ src/main/resources/static/js/manager/userData.js
@@ -1,16 +1,70 @@
-const userId = document.getElementById("userId").value;
-const linkResponseButtons = document.querySelectorAll(".linkResponseList");
+// const userId = document.getElementById("userId").value;
+// const linkResponseButtons = document.querySelectorAll(".linkResponseList");
 
-linkResponseButtons.forEach(button => {
-  button.addEventListener('click', function () {
-    const idUser = this.getAttribute("data-iduser");
-    const nrNumber = this.getAttribute("data-nrnumber");
+// linkResponseButtons.forEach(button => {
+//   button.addEventListener('click', function () {
+//     const idUser = this.getAttribute("data-iduser");
+//     const nrNumber = this.getAttribute("data-nrnumber");
     
-    goToListResponses(idUser, nrNumber);
-  });
+//     goToListResponses(idUser, nrNumber);
+//   });
+// });
+
+// function goToListResponses(idUser, nrNumber) {
+//   window.location.href = `/user/infoClient/${idUser}/${nrNumber}`;
+// }
+
+
+
+const btnOptions = document.getElementById("btnOptionUser").addEventListener('click',showModalOptionsUser)
+
+let selectedUserId = null;
+let modalVisible = false;
+
+
+
+document.addEventListener('click', function(event) {
+  const btnOptions = document.getElementById('options');
+  const target = event.target;
+
+  if (modalVisible && !btnOptions.contains(target) && !target.closest('button')) {
+      hideModalOptionsUser();
+  }
+
 });
 
-function goToListResponses(idUser, nrNumber) {
-  window.location.href = `/user/infoClient/${idUser}/${nrNumber}`;
+
+function showModalOptionsUser(button){
+  const options = document.getElementById('options');
+  const rect = button.getBoundingClientRect();
+  selectedUserId = button.closest('tr').getAttribute('data-id');
+  userIdFormInput.value = selectedUserId;
+
+  const marginLeft = 125;
+  const marginTop = -40;
+
+  let modalLeft = rect.left - marginLeft;
+  let modalTop = rect.top + marginTop;
+
+  options.style.top = `${modalTop}px`;
+  options.style.left = `${modalLeft}px`;
+
+  options.style.display = 'block';
+  modalVisible = true;
 }
 
+function hideModalOptionsUser(){
+  const options = document.getElementById('options');
+  options.style.display="none";
+}
+
+
+
+
+function redirectToInfo(){
+  window.location.href="/user/infoClient/"+selectedUserId;
+}
+
+function redirectToCertificados(){
+  window.location.href="/user/infoClient/"+selectedUserId;
+}
