bb1627c36e6604f1087a9c48b4d51f0539a632b7
Brxnin-FX <brunosantiago7002@gmail.com>
2025-04-17 08:57:13 -0300

    teste remove Dockerfile

--- target/classes/static/js/manager/showMenu.js
+++
@@ -1,9 +0,0 @@
-function showMenu(){
-    const menuContent = document.getElementById('dropdownMenu');
-
-    if(menuContent.style.display==='none' || menuContent.style.display===''){
-        menuContent.style.display='block';
-    }else{
-        menuContent.style.display='none';
-    }
-}
