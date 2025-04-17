451bdae257106a9e138175f5c967245a11dce4ff
Brxnin-FX <brunosantiago7002@gmail.com>
2025-04-17 13:46:30 -0300

    teste

--- target/classes/static/js/client/player.js
+++
@@ -1,7 +0,0 @@
-const btnRealizeForm = document.getElementById("btnStartForm");
-let nrNumber = null;
-
-function RealizeForm(button) {
-    nrNumber = document.getElementById("nrNumberValue").value
-    window.location.href = `/question/${nrNumber}`;
-}
